9ff89e52ae0afe3f07c433c050e557cc04f51ba2
kadalihiranmayee <hiranmayeekadali22@gmail.com>
2022-03-11 19:16:14 +0530

    Initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,21 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    public static Calculator calculator;
+
+    @BeforeClass
+    public static void setup() {
+        calculator = new Calculator();
+    }
+
+    //Bhargava Bhamidipati TestCase 1
+    @Test
+    public void testAdd() {
+        assertEquals(4, calculator.add(2, 2));
+        assertEquals(0, calculator.add(2, -2));
+        assertEquals(-4, calculator.add(-2, -2));
+    }
+}
